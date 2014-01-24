16f74a4aeb2256348ea99f7fee5d363c9a8492f8
Alexander Biskop <biskop@stocard.de>
2014-01-24 19:01:55 +0100

    made geoiplookup cmdline command conf'able

--- conf/config.js
+++ conf/config.js
@@ -3,5 +3,9 @@ return module.exports = {
   service: {
     port: 7000,
     publicBaseUrl: 'http://localhost:7000',
+  },
+  geoiplookup_cli: {
+    // TODO: this should be something like "geoiplookup -f somefile" once DB file is set up via cron/wget
+    command: 'geoiplookup '
   }
 }
